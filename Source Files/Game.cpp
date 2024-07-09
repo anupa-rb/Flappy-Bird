f78df86fe0abfa88da86b2801db5ef46d9cd2627
Anupa Ranabhat <anupa.ranabhat@gmail.com>
2024-07-09 09:01:12 +0545

    Changes in main.cpp

--- Source Files/Game.cpp
+++ Source Files/Game.cpp
@@ -1,4 +1,23 @@
-class Game
-{
+#include"Game.h"
+// Game::Game(sf::RenderWindow &window): win(window)
+// {
 
-};
+// }
+void Game::startGameLoop()
+{
+    //Game Loop
+    while(win.isOpen())
+    {
+        //Event Loop
+        sf::Event event;
+        while(win.pollEvent(event))
+        {
+            if(event.type==sf::Event::Closed)
+            {
+                win.close();
+            }
+        }
+        //display window
+        win.display();
+    }
+}
--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,25 +1,35 @@
+// #include <SFML/Graphics.hpp>
+// #include"headers.h"
+// #include"Game.h"
+
+// int main()
+// {
+//     sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Flappy Bird");
+//     Game game(window);
+//     game.startGameLoop();
+//     return 0;
+// }
 #include <SFML/Graphics.hpp>
-#include"headers.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Flappy Bird");
-    
-    // shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Green);
 
-    // while (window.isOpen())
-    // {
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    //     window.clear();
-    //     window.draw(shape);
-    //     window.display();
-    // }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
